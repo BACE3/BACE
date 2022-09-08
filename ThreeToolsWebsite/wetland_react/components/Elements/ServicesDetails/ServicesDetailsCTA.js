a53d449358e46464200a292bdaec8094c2a35f90
Alyssa Chew <85897001+alyssabchew@users.noreply.github.com>
2022-09-08 00:45:35 -0700

    update cta in docs details

--- ThreeToolsWebsite/wetland_react/components/Elements/ServicesDetails/ServicesDetailsCTA.js
+++ ThreeToolsWebsite/wetland_react/components/Elements/ServicesDetails/ServicesDetailsCTA.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function ServicesDetailsCTA() {
     return (
@@ -11,22 +12,22 @@ export default function ServicesDetailsCTA() {
                   <div className="col-xxl-8 offset-xxl-2">
                      <div className="cta__content-4 text-center">
                         <div className="section__title-wrapper section__title-wrapper-4 section__title-white text-center mb-45 wow fadeInUp" data-wow-delay=".3s">
-                           <h2 className="section__title section__title-4">Our Free Trial for 14-days Today</h2>
-                           <p>Get the word out and sell more with sleek email messages that.</p>
+                           <h2 className="section__title section__title-4">See something missing from our documentation?</h2>
+                           <Link href='/contact'><p>Contact us with the changes!</p></Link>
                         </div>
-                        <div className="cta__form mb-25 wow fadeInUp" data-wow-delay=".5s">
+                        {/* <div className="cta__form mb-25 wow fadeInUp" data-wow-delay=".5s">
                            <form action="#">
                               <input type="email" placeholder="Enter Your Email" />
                               <button type="submit">Start Free Trial</button>
                            </form>
-                        </div>
-                        <div className="cta__features wow fadeInUp" data-wow-delay=".7s">
+                        </div> */}
+                        {/* <div className="cta__features wow fadeInUp" data-wow-delay=".7s">
                            <ul>
                               <li>Product support</li>
                               <li>Free trial</li>
                               <li>Connect Customer</li>
                            </ul>
-                        </div>
+                        </div> */}
                      </div>
                   </div>
                </div>
